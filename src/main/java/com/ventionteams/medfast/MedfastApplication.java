bd2147a858f531cd69d3728485dfe90436d6626a
David Rabko <davidryabko13@gmail.com>
2024-07-02 21:05:57 +0200

    Add spring boot project

---
+++ src/main/java/com/ventionteams/medfast/MedfastApplication.java
@@ -0,0 +1,13 @@
+package com.ventionteams.medfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedfastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedfastApplication.class, args);
+    }
+
+}
