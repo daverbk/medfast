2ad0ca163cbfd96371a5066cc791e28565377086
kasiaOsowska <kasia@osowski.info>
2024-07-31 17:36:01 +0200

    MED-246: Update the standard response class with a description

--- src/main/java/com/ventionteams/medfast/dto/response/StandardizedResponse.java
+++ src/main/java/com/ventionteams/medfast/dto/response/StandardizedResponse.java
@@ -1,5 +1,6 @@
 package com.ventionteams.medfast.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +10,25 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Standardized response structure")
 public class StandardizedResponse<T> {
+
+    @Schema(description = "Value of generic type T", example = "JwtAuthenticationResponse")
     private T data;
+
+    @Schema(description = "HTTP status code", example = "200")
     private int status;
+
+    @Schema(description = "Message", example = "Sign up successful")
     private String message;
+
+    @Schema(description = "Timestamp", example = "2024-07-31T14:00:00.0000000")
     private LocalDateTime timestamp;
+
+    @Schema(description = "Error class", example = "com.ventionteams.medfast.exception.auth.UserIsAlreadyVerifiedException")
     private String errorClass;
+
+    @Schema(description = "Error message", example = "User with credential [johndoe@gmail.com]: User is already verified")
     private String errorMessage;
 
     public StandardizedResponse(T data, int status, String message) {
@@ -31,4 +45,5 @@ public class StandardizedResponse<T> {
         this.message = message;
         this.timestamp = LocalDateTime.now();
     }
+
 }
