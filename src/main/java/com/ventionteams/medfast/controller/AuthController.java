ab85d365dbe590f57ffcd621bccf1512cf11d10c
kasiaOsowska <kasia@osowski.info>
2024-07-31 17:39:03 +0200

    MED-246: Update Controller to use new response structure

--- src/main/java/com/ventionteams/medfast/controller/AuthController.java
+++ src/main/java/com/ventionteams/medfast/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.ventionteams.medfast.dto.request.RefreshTokenRequest;
 import com.ventionteams.medfast.dto.request.SignInRequest;
 import com.ventionteams.medfast.dto.request.SignUpRequest;
 import com.ventionteams.medfast.dto.response.JwtAuthenticationResponse;
+import com.ventionteams.medfast.dto.response.StandardizedResponse;
 import com.ventionteams.medfast.service.auth.AuthenticationService;
 import com.ventionteams.medfast.service.auth.RefreshTokenService;
 import com.ventionteams.medfast.service.auth.VerificationTokenService;
@@ -15,6 +16,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+//exceptions
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.mail.MailAuthenticationException;
+import com.ventionteams.medfast.exception.auth.UserAlreadyExistsException;
+import org.springframework.security.authentication.DisabledException;
+import com.ventionteams.medfast.exception.auth.TokenNotFoundException;
+import com.ventionteams.medfast.exception.auth.UserIsAlreadyVerifiedException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.io.IOException;
 
@@ -29,52 +40,134 @@ public class AuthController {
 
     @Operation(summary = "Sign up")
     @PostMapping("/signup")
-    public ResponseEntity<String> signUp(@RequestBody @Valid SignUpRequest request) {
-        ResponseEntity<String> response;
+    public ResponseEntity<StandardizedResponse<String>> signUp(@RequestBody @Valid SignUpRequest request) {
+        StandardizedResponse<String> response;
 
         try {
-            response = ResponseEntity.ok(authenticationService.signUp(request));
-        } catch (MessagingException | IOException e) {
-            response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("We ran into an issue while sending a verification email, try again please");
+            String signupResponse = authenticationService.signUp(request);
+            response =
+                new StandardizedResponse<>(
+                    signupResponse,
+                    HttpStatus.OK.value(),
+                    "Sign up successful");
+        } catch (MessagingException | IOException |
+                 MailAuthenticationException | UserAlreadyExistsException e) {
+            // When MailAuthenticationException is thrown, it means that the mail is not sent but the user is created and saved in database
+            response =
+                new StandardizedResponse<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    "We ran into an issue while sending a verification email, try again please",
+                    e.getClass().getName(),
+                    e.getMessage());
         }
 
-        return response;
+        return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @Operation(summary = "Sign in")
     @PostMapping("/signin")
-    public ResponseEntity<JwtAuthenticationResponse> signIn(@RequestBody @Valid SignInRequest request) {
-        return ResponseEntity.ok(authenticationService.signIn(request));
+    public ResponseEntity<StandardizedResponse<JwtAuthenticationResponse>> signIn(@RequestBody @Valid SignInRequest request) {
+        StandardizedResponse<JwtAuthenticationResponse> response;
+
+        try{
+            JwtAuthenticationResponse authenticationResponse = authenticationService.signIn(request);
+            response =
+                new StandardizedResponse<>(
+                    authenticationResponse,
+                    HttpStatus.OK.value(),
+                    "Sign in successful");
+        } catch (BadCredentialsException | DisabledException e) {
+            response =
+                new StandardizedResponse<>(
+                    HttpStatus.UNAUTHORIZED.value(),
+                    "Provided credentials are bad or user is disabled",
+                    e.getClass().getName(),
+                    e.getMessage());
+        }
+
+        return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @Operation(summary = "Refresh access token")
     @PostMapping("/refresh")
-    public ResponseEntity<JwtAuthenticationResponse> refreshToken(@RequestBody @Valid RefreshTokenRequest request) {
-        return ResponseEntity.ok(refreshTokenService.refreshToken(request));
+    public ResponseEntity<StandardizedResponse<JwtAuthenticationResponse>> refreshToken(@RequestBody @Valid RefreshTokenRequest request) {
+        StandardizedResponse<JwtAuthenticationResponse> response;
+        try{
+            JwtAuthenticationResponse refreshResponse = refreshTokenService.refreshToken(request);
+            response =
+                new StandardizedResponse<>(
+                    refreshResponse,
+                    HttpStatus.OK.value(),
+                    "Refreshing token successful");
+        } catch (Exception e) {
+            response =
+                new StandardizedResponse<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    "We ran into an issue while refreshing your token, try again please",
+                    e.getClass().getName(),
+                    e.getMessage());
+        }
+        return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @Operation(summary = "Verify email address")
     @PostMapping("/verify")
-    public ResponseEntity<String> verifyUser(@RequestParam("email") String email, @RequestParam("code") String code) {
-        return verificationTokenService.verify(email, code)
-            ? ResponseEntity.ok("Your account is verified")
-            : ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid verification code");
+    public ResponseEntity<StandardizedResponse<String>> verifyUser(@RequestParam("email") String email, @RequestParam("code") String code) {
+        StandardizedResponse<String> response;
+        try {
+            boolean isVerified = verificationTokenService.verify(email, code);
+
+
+            if (isVerified) {
+                response = new StandardizedResponse<>(
+                    "Your account is verified",
+                    HttpStatus.OK.value(),
+                    "Operation successful");
+            } else {
+                response = new StandardizedResponse<>(
+                    HttpStatus.BAD_REQUEST.value(),
+                    "Invalid verification code",
+                    null,
+                    null);
+            }
+        }
+        catch (TokenNotFoundException | UsernameNotFoundException e) {
+            response =
+                new StandardizedResponse<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    "We ran into an issue while verifying your email, try again please",
+                    e.getClass().getName(),
+                    e.getMessage());
+
+        }
+
+        return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @Operation(summary = "Request another verification email")
     @PostMapping("/reverify")
-    public ResponseEntity<String> reverifyUser(@RequestParam("email") String email) {
-        ResponseEntity<String> response;
+    public ResponseEntity<StandardizedResponse<String>> reverifyUser(@RequestParam("email") String email) {
+        StandardizedResponse<String> response;
 
         try {
             authenticationService.sendVerificationEmail(email);
-            response = ResponseEntity.ok("Another email has been sent to your email");
-        } catch (MessagingException | IOException e) {
-            response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("We ran into an issue while sending a verification email, try again please");
+            String reverifyResponse = "Another email has been sent to your email";
+            response = new StandardizedResponse<>(
+                reverifyResponse,
+                HttpStatus.OK.value(),
+                "Operation successful");
+        } catch (MessagingException | IOException | UsernameNotFoundException |
+                 IncorrectResultSizeDataAccessException | UserIsAlreadyVerifiedException e) {
+            // IncorrectResultSizeDataAccessException is a wierd bug where each time it returns incremented
+            // amount of users even though there is only one user with that email in database
+            response =
+                new StandardizedResponse<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    "We ran into an issue while sending another verification email, try again please",
+                    e.getClass().getName(),
+                    e.getMessage());
         }
 
-        return response;
+        return ResponseEntity.status(response.getStatus()).body(response);
     }
 }
